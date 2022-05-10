ace82cb0f268e6897196a6b2afe591222ee9a698
WUMANHO <wumanho@tutanota.com>
2022-05-10 22:46:58 +0800

    init

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- 基础设置
+require('basic')
+-- 快捷键映射
+require("keybindings")
+-- Packer 插件管理
+require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,26 @@
+local packer = require("packer")
+packer.startup({
+  function(use)
+    -- Packer 可以管理自己本身
+    use 'wbthomason/packer.nvim'
+    -- 你的插件列表...
+  end,
+  config = {
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "single" })
+        end,
+    },
+  },
+})
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
+
